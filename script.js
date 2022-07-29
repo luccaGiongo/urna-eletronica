f86db48cbb7338ae8cffd6fda945c79950335c8b
Lucca <giongo.lucca@gmail.com>
2022-07-29 11:49:20 -0300

    Funções botoes (branco, corrige e confima)

--- script.js
+++ script.js
@@ -11,12 +11,15 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
-
+let votoBranco = false;
+let votos = [];
 
 // Funções
 function comecarEtapa(){
     let etapa = etapas[etapaAtual];
     let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
 
     for(let i=0; i<etapa.numeros; i++){
         if(i === 0){
@@ -50,8 +53,9 @@ function atualizaInterface(){
         descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
         let fotosHtml = '';
         for(let i in candidato.fotos){
-            fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
 
+
+            fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
         };
         lateral.innerHTML = fotosHtml;
     }else{
@@ -59,7 +63,6 @@ function atualizaInterface(){
         aviso.style.display = 'block';
         descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
     }
-    
 }
 
 function clicou(n){
@@ -77,13 +80,47 @@ function clicou(n){
 }
 
 function branco(){
-    alert("Clicou em Branco")
+    if(numero === ''){
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML =  '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    } else {
+        alert("Voto em Branco permitido somente sem números preenchidos");
+    };
 }
+
 function corrige(){
-    alert("Clicou em Corrige")
+    comecarEtapa();
 }
+
 function confirma(){
-    alert("Clicou em Confirma")
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false;
+    if(votoBranco === true){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
+    } else if(numero.length === etapa.numeros) {
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
+    }
+
+    if(votoConfirmado){
+        etapaAtual ++;
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        } else {
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+            console.log(votos);
+        }
+    }
 }
 
 comecarEtapa();
